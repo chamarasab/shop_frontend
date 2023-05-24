a40cfcd395842e21c82f627721995970554b8370
chamarasab <chamara.sab@outlook.com>
2023-05-24 13:58:05 +0530

    create new route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AllStocksComponent } from './stocks/all-stocks/all-stocks.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: AllStocksComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
