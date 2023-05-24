1deb63bda0219d8d8dd9b47cc70e310f4f31bb55
chamarasab <chamara.sab@outlook.com>
2023-05-24 14:09:35 +0530

    set url to get data

--- src/app/stocks/stocks.service.ts
+++ src/app/stocks/stocks.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
+import { Stocks } from './stocks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StocksService {
-  constructor(private http:HttpClient) { }
+  constructor(private http: HttpClient) { }
+
+  get() {
+    return this.http.get<Stocks[]>("http://localhost:3000/stocks");
+  }
 }
+
