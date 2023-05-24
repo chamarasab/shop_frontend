8d373fcf6840d72dc7e3ac56968937b58f95fbd5
chamarasab <chamara.sab@outlook.com>
2023-05-24 16:08:52 +0530

    send posted data and router back

--- src/app/stocks/add-stocks/add-stocks.component.ts
+++ src/app/stocks/add-stocks/add-stocks.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CreateStock } from '../create-stock';
+import { StocksService } from '../stocks.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-stocks',
   templateUrl: './add-stocks.component.html',
   styleUrls: ['./add-stocks.component.css']
 })
-export class AddStocksComponent {
+export class AddStocksComponent implements OnInit {
 
+  constructor(private stockService: StocksService, private router: Router) { }
+
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+
+  stocks: CreateStock = {
+    name: '',
+    price: '',
+    quantity: ''
+  }
+
+  create() {
+    this.stockService.create(this.stocks).subscribe(() => {
+      this.router.navigate(['/']);
+    });
+  }
 }
