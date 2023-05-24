bfebf541fc5a6a27ad12a6988fbd2b2cf4d93fd9
chamarasab <chamara.sab@outlook.com>
2023-05-24 15:43:19 +0530

    create new path to add stock

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AllStocksComponent } from './stocks/all-stocks/all-stocks.component';
+import { AddStocksComponent } from './stocks/add-stocks/add-stocks.component';
 
 const routes: Routes = [
   {
     path: '',
     component: AllStocksComponent
+  },
+  {
+    path: 'add-stock',
+    component: AddStocksComponent
   }
 ];
 
