a67662e6261cc52c0aacd739879d11a446fde46f
chamarasab <chamara.sab@outlook.com>
2023-06-04 16:21:34 +0530

    read the id of the url

--- src/app/stocks/edit-stocks/edit-stocks.component.ts
+++ src/app/stocks/edit-stocks/edit-stocks.component.ts
@@ -12,7 +12,7 @@ export class EditStocksComponent implements OnInit{
   }
   ngOnInit(): void {
     this.route.paramMap.subscribe((params)=>{
-      params.get('')
+      params.get('id')
     })
   }
 }
