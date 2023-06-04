2416c638662fe7ba67232f169eae5efbad79c058
chamarasab <chamara.sab@outlook.com>
2023-06-04 16:38:30 +0530

    response mapping

--- src/app/stocks/edit-stocks/edit-stocks.component.ts
+++ src/app/stocks/edit-stocks/edit-stocks.component.ts
@@ -1,5 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute} from "@angular/router";
+import {StocksService} from "../stocks.service";
+import {CreateStock} from "../create-stock";
 
 @Component({
   selector: 'app-edit-stocks',
@@ -7,11 +9,24 @@ import {ActivatedRoute} from "@angular/router";
   styleUrls: ['./edit-stocks.component.css']
 })
 export class EditStocksComponent implements OnInit{
-  constructor(private route:ActivatedRoute) {}
-  itemId:String = '';
+  constructor(private route:ActivatedRoute, private stocksService:StocksService) {}
+  itemId:string = '';
+  stocks: CreateStock = {
+    name: '',
+    price: '',
+    quantity: ''
+  }
   ngOnInit(): void {
     this.route.paramMap.subscribe((params)=>{
       this.itemId = params.get('id') ?? ""
+    })
+  }
+
+  getById() {
+    this.stocksService.getById(this.itemId).subscribe((data)=>{
+      this.stocks.name = data.name;
+      this.stocks.price = data.price;
+      this.stocks.quantity = data.quantity;
     })
   }
 }
