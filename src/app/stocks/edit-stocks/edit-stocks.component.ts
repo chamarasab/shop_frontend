ca46526628ee1093844f318ffbc1d8b7ea5416af
chamarasab <chamara.sab@outlook.com>
2023-06-04 16:08:12 +0530

    read the id of the url

--- src/app/stocks/edit-stocks/edit-stocks.component.ts
+++ src/app/stocks/edit-stocks/edit-stocks.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-edit-stocks',
   templateUrl: './edit-stocks.component.html',
   styleUrls: ['./edit-stocks.component.css']
 })
-export class EditStocksComponent {
+export class EditStocksComponent implements OnInit{
+  constructor(private route:ActivatedRoute) {
 
+  }
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params)=>{
+      params.get('')
+    })
+  }
 }
